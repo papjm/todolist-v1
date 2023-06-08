4da6627fcc4736abcc9388317683b6b55341d0e9
papjm <102404994+papjm@users.noreply.github.com>
2023-06-08 14:29:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const app = express();
 
 app.get("/", function(req, res) {
 var today = new Date();
-if (today.getDay() === 6 || today.getDay() === 0)
+if (today.getDay() === 6 || today.getDay() === 0) {
+  res.send("yay it's the weekened")
+}
+
 
 });
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
+
