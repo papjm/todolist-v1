4996e42854431561590aef273b3c4ac8e1826713
papjm <102404994+papjm@users.noreply.github.com>
2023-06-08 14:14:26 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+///jshint  esversion:6
+const express = require("express ");
+const bodyParser = require("bodyParser");
+
+const app = express();
+
+app.get("/", function() {
+  res.send("Hello");
+
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000")
+})
